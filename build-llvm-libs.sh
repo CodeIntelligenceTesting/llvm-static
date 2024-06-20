329e41a65dd0f3f45f2361f859fc4a5e205fe268
Simon Resch <simon.resch@code-intelligence.de>
2024-06-20 08:21:45 +0200

    feat: add macos LLVM build

--- build-llvm-libs.sh
+++ build-llvm-libs.sh
@@ -4,9 +4,25 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BUILD_DIR="$SCRIPT_DIR/build"
 LLVM_BUILD_DIR="$BUILD_DIR/external.llvm-project"
 
+OUTPUT_FILE=llvm-static-libs.tar.xz
+if [ "$#" -eq 1 ]
+then
+  OUTPUT_FILE=$1
+fi
+
+CC=clang
+CXX=clang++
+
+TARGET_PLATFORM=X86
+if [ "$(uname)" == "Darwin" ]; then
+  # On Mac we only support the new apple silicon architecture.
+  TARGET_PLATFORM=AArch64
+fi
+
 cd "$SCRIPT_DIR"
-mkdir -p build
-cmake -B build -GNinja .
+mkdir -p $BUILD_DIR
+
+cmake -B build -GNinja -DLLVM_TARGETS_TO_BUILD=${TARGET_PLATFORM} .
 cmake --build build
 
 # Pack everything we need into a tar archive.
@@ -14,4 +30,4 @@ cmake --build build
 # a CMake file that makes it easy to link libclangTooling.
 cp $SCRIPT_DIR/cmake/CMakeLists.txt $LLVM_BUILD_DIR
 cd $LLVM_BUILD_DIR
-tar -czvf "$SCRIPT_DIR/llvm-static-libs.tar.xz" include/ lib/*.a CMakeLists.txt
+tar -czvf "$SCRIPT_DIR/$OUTPUT_FILE" include/ lib/*.a CMakeLists.txt
